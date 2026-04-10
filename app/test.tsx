c269db07ab7161960777990af0cecc0cf4b74e00
niloofarmostafazadeh <nilfar1379@gmail.com>
2026-04-10 14:39:27 +0330

    dtabase

---
+++ app/test.tsx
@@ -0,0 +1,15 @@
+import { supabase } from '../lib/supabase'
+
+export default async function TestPage() {
+  const { data, error } = await supabase.from('test').select('*')
+
+  console.log("DATA:", data)
+  console.log("ERROR:", error)
+
+  return (
+    <div>
+      <h1>Supabase Test Page</h1>
+      <p>Check terminal / console</p>
+    </div>
+  )
+}
